ac958d55e50b1522f38d26a6a47f20a16c28795e
222764600 <222764600@mycput.ac.za>
2024-08-28 22:27:19 +0200

    Shipping and Inventory

---
+++ src/main/java/za/ac/cput/controller/InventoryController.java
@@ -0,0 +1,36 @@
+package za.ac.cput.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.Inventory;
+import za.ac.cput.services.InventoryService;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/inventory")
+public class InventoryController {
+
+    @Autowired
+    private InventoryService inventoryService;
+
+    @PostMapping("/create")
+    public Inventory create(@RequestBody Inventory inv){
+        return InventoryService.create(inv);
+    }
+
+    @GetMapping("/read/{invId}")
+    private Inventory read(@PathVariable String invId){
+        return inventoryService.read(invId);
+    }
+
+    @PostMapping("/update")
+    private Inventory update(@RequestBody Inventory inv){
+        return inventoryService.update(inv);
+    }
+
+    @GetMapping("/getall")
+    private List<Inventory>getall(){
+        return inventoryService.getall();
+    }
+}
